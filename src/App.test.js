dd04a39ceb196b1d019caf4d9c7919722dacc367
vineeth lakkineni <vineethlakkineni.js@gmail.com>
2022-01-19 10:32:51 -0400

    update test case and snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,34 @@ import { shallow } from "enzyme";
 import App from "./App";
 
 describe("MyComponent", () => {
-	it('should render correctly in "debug" mode', () => {
-		const component = shallow(<App />);
-		expect(component).toMatchSnapshot();
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<App />);
+	});
+	afterEach(() => {
+		jest.useRealTimers();
+	});
+	it("should render correctly", () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+	it("should have an input field", () => {
+		expect(wrapper.find("input").length).toEqual(1);
+	});
+	it("should render input and match value", () => {
+		wrapper.find("input").simulate("change", {
+			target: {
+				value: "abc",
+			},
+		});
+		expect(wrapper.find("input").prop("value")).toEqual("abc");
+	});
+	it("should simulate keypress - enter key with empty", () => {
+		wrapper.find("input").simulate("change", {
+			target: {
+				value: "",
+			},
+		});
+		wrapper.find("input").simulate("keypress", { key: "Enter" });
+		expect(wrapper.find(".book").length).toEqual(0);
 	});
 });
